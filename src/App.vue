d41f0a32c3a70b8ac2f9c6e73a7c1c8c279423da
yangjiafeng <yang.yjf@aliyun.com>
2023-03-13 18:49:38 +0800

    style: add tailwind css

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,54 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue';
-const a = '1';
-const b = 1;
-const c = { a: 1, c: 3 };
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="chat-notification">
+    <div class="chat-notification-logo-wrapper">
+      <img class="chat-notification-logo" src="/vite.svg" alt="ChitChat Logo" />
+    </div>
+    <div class="chat-notification-content">
+      <h4 class="chat-notification-title text-rose-800">ChitChat</h4>
+      <p class="chat-notification-message">You have a new message!</p>
+      <p class="text-rose-800 hover:text-sky-400">You have a new message!</p>
+      <p class="text-sky-400/100">The quick brown fox...</p>
+      <p class="text-sky-400/75">The quick brown fox...</p>
+      <p class="text-sky-400/50">The quick brown fox...</p>
+      <p class="text-sky-400/25">The quick brown fox...</p>
+      <p class="text-sky-400/0">The quick brown fox...</p>
+      <p class="text-blue-600/50 bg-green-500">Thkkjkjkjkjk..</p>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.chat-notification {
+  display: flex;
+  max-width: 24rem;
+  margin: 0 auto;
+  padding: 1.5rem;
+  border-radius: 0.5rem;
+  background-color: #fff;
+  box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.1),
+    0 10px 10px -5px rgba(0, 0, 0, 0.04);
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.chat-notification-logo-wrapper {
+  flex-shrink: 0;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.chat-notification-logo {
+  height: 3rem;
+  width: 3rem;
+}
+.chat-notification-content {
+  margin-left: 1.5rem;
+  padding-top: 0.25rem;
+}
+.chat-notification-title {
+  color: #1a202c;
+  font-size: 1.25rem;
+  line-height: 1.25;
+}
+.chat-notification-message {
+  color: #718096;
+  font-size: 1rem;
+  line-height: 1.5;
 }
 </style>
---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // Example content paths...
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
